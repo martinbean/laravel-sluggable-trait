0d094a5cd99bb48a12117ebfb3008262da721e87
Martin Bean <martin@martinbean.co.uk>
2017-03-30 15:30:02 +0100

    Re-factor

--- README.md
+++ README.md
@@ -9,34 +9,76 @@ A trait you can apply to Eloquent models to have slugs automatically generated o
 ## Usage
 
 ```php
-<?php namespace App;
+<?php
+
+namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use MartinBean\Database\Eloquent\Sluggable;
 
-class Item extends Model {
-
-	use Sluggable;
-
+class Item extends Model
+{
+    use Sluggable;
 }
 ```
 
 By default, the trait assumes your database has two columns: `name` and `slug`.
-If you need to change these, you can do so via class constants:
+If you need to change these, you can override the getter methods:
 
 ```php
-<?php namespace App;
+<?php
+
+namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use MartinBean\Database\Eloquent\Sluggable;
 
-class Item extends Model {
+class Item extends Model
+{
+    use Sluggable;
 
-	use Sluggable;
+    public function getSlugColumnName()
+    {
+        return 'seo_title';
+    }
 
-	const DISPLAY_NAME = 'headline';
-	const SLUG = 'seo_url';
+    public function getSluggableString()
+    {
+        return 'headline';
+    }
+}
+```
 
+The `getSlugColumnName()` method should return the name of the column you want
+to store slugs in your database table.
+
+The `getSluggableString()` should return a string you want to create a slug
+from. This is exposed as a method and not a property or constantly as you may
+want to create a slug from the value of one than one column. For example:
+
+```php
+/**
+ * Create a string based on the first and last name of a person.
+ */
+public function getSluggableString()
+{
+    return sprintf('%s %s', $this->first_name, $this->last_name);
+}
+```
+
+```php
+/**
+ * Create a string based on a formatted address string.
+ */
+public function getSluggableString()
+{
+    return implode(', ', array_filter([
+        $this->street_address,
+        $this->locality,
+        $this->region,
+        $this->postal_code,
+        $this->country,
+    ]));
 }
 ```
 
--- src/Sluggable.php
+++ src/Sluggable.php
@@ -1,46 +1,66 @@
-<?php namespace MartinBean\Database\Eloquent;
+<?php
+
+namespace MartinBean\Database\Eloquent;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
-trait Sluggable {
+trait Sluggable
+{
+    /**
+     * Boot the sluggable trait for a model.
+     *
+     * @return void
+     */
+    public static function bootSluggable()
+    {
+        static::saving(function (Model $model) {
+            if (empty($model->getSlug())) {
+                $model->setSlug(Str::slug($model->getSluggableString()));
+            }
+        });
+    }
 
-	/**
-	 * Boot the sluggable trait for a model.
-	 *
-	 * @return void
-	 */
-	public static function bootSluggable()
-	{
-		$name = static::getDisplayNameColumn();
-		$slug = static::getSlugColumn();
+    /**
+     * The name of the column to use for slugs.
+     *
+     * @return string
+     */
+    public function getSlugColumnName()
+    {
+        return 'slug';
+    }
 
-		$callback = function(Model $model) use ($name, $slug)
-		{
-			$model->$slug = Str::slug($model->$name);
-		};
+    /**
+     * Get the current slug value.
+     *
+     * @return string
+     */
+    public function getSlug()
+    {
+        return $this->getAttribute($this->getSlugColumnName());
+    }
 
-		static::registerModelEvent('saving', $callback, 0);
-	}
+    /**
+     * Set the slug to the given value.
+     *
+     * @param  string  $value
+     * @return $this
+     */
+    public function setSlug($value)
+    {
+        $this->setAttribute($this->getSlugColumnName(), $value);
 
-	/**
-	 * Get the name of the display name column.
-	 *
-	 * @return string
-	 */
-	public static function getDisplayNameColumn()
-	{
-		return defined('static::DISPLAY_NAME') ? static::DISPLAY_NAME : 'name';
-	}
+        return $this;
+    }
 
-	/**
-	 * Get the name of the slug column.
-	 *
-	 * @return string
-	 */
-	public static function getSlugColumn()
-	{
-		return defined('static::SLUG') ? static::SLUG : 'slug';
-	}
-
+    /**
+     * Get the string to create a slug from.
+     *
+     * @return string
+     */
+    public function getSluggableString()
+    {
+        return $this->getAttribute('name');
+    }
 }
